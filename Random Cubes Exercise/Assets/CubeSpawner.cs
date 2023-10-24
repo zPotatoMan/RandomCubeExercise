50fabbeade78d1bc7089609d624a0065fec21c66
zPotatoMan <abp8171@utulsa.edu>
2023-10-24 11:25:42 -0500

    final push

---
+++ Random Cubes Exercise/Assets/CubeSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cubePrefabVar;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiate(cubePrefabVar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Instantiate(cubePrefabVar);
+
+        GameObject cubeGO = Instantiate<GameObject>(cubePrefabVar);
+        Material mat = cubeGO.GetComponent<Renderer>().material;
+        Color color = Random.ColorHSV();
+        mat.color = color;
+    }
+}
